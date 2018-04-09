a44c3961c2e889899d5fe3cc14a8ff33a9b89390
Asaad Khattab <asaad1592@gmail.com>
2018-04-08 23:10:34 -0400

    salt and hash

--- models/user.js
+++ models/user.js
@@ -1,11 +1,14 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+
 var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     //Ensure there is not duplication in the email
     unique: true,
     required: true,
-    //Removes whitespace 
+    //Removes whitespace
     trim: true
   },
   name: {
@@ -25,6 +28,18 @@ var UserSchema = new mongoose.Schema({
     required:true
   }
 
+});
+//PRESAVE HOOK
+  //Runs before saving record! - middleware
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err,hash) {
+    if(err){
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
 });
 
 //EXPORT Schema
